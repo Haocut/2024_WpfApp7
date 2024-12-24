4b6ae763498d750d3e4bc2252ed3a5cf54a7fc9b
Haocut <hao891798@gmail.com>
2024-12-24 17:40:30 +0800

    新增 LiveCharts.Wpf 套件及 AQIData 類別
    在 `2024_WpfApp7.csproj` 中新增了 `LiveCharts.Wpf` 套件的引用。
    在 `MainWindow.xaml` 中新增了 `LiveCharts.Wpf` 的命名空間引用，並在 Grid 中新增了多個 RowDefinitions 和 Borders，其中一個 Border 包含了一個 `CartesianChart` 控制項。
    在 `MainWindow.xaml.cs` 中移除了多個不必要的 using 指令，並新增了 `System.Net.Http` 的引用。修正了 `defultURL` 的拼寫錯誤為 `defaultURL`，並將 `GetAQIButton_Click` 方法改為非同步方法，新增了 `FetchContentAsync` 方法來抓取資料。
    新增了一個名為 `AQIData.cs` 的檔案，並在其中定義了一個 `AQIData` 類別。

--- 2024_WpfApp7/MainWindow.xaml.cs
+++ 2024_WpfApp7/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
+﻿using System.Net.Http;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace _2024_WpfApp7
 {
@@ -16,16 +8,41 @@ namespace _2024_WpfApp7
     /// </summary>
     public partial class MainWindow : Window
     {
-        string defultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
+        string defaultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
-            UrlTextBox.Text = defultURL;
+            UrlTextBox.Text = defaultURL;
         }
 
-        private void GetAQIButton_Click(object sender, RoutedEventArgs e)
+        private async void GetAQIButton_Click(object sender, RoutedEventArgs e)
         {
             ContentTextBox.Text = "抓取資料中...";
+
+            string data = await FetchContentAsync(defaultURL);
+            ContentTextBox.Text = data;
+
+        }
+
+        private async Task<string> FetchContentAsync(string url)
+        {
+            using (var client = new HttpClient())
+            {
+                client.Timeout = TimeSpan.FromSeconds(100);
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                catch (HttpRequestException e)
+                {
+                    MessageBox.Show($"Request exception: {e.Message}");
+                    return null;
+                }
+            }   
+
         }
     }
 }
