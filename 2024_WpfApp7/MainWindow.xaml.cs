7b0c0e8eff64f2b2f838a917ef542300c5dade6c
Haocut <hao891798@gmail.com>
2024-12-24 15:43:58 +0800

    調整視窗大小並更新Grid佈局
    將視窗大小調整為1000x1200。新增Grid的RowDefinitions和ColumnDefinitions，定義三行三列佈局。新增StackPanel、TextBox、Button、GridSplitter和StatusBar等UI元素。新增預設URL變數`defultURL`，並在建構函式中設定UrlTextBox的文字。新增`GetAQIButton_Click`事件處理函式，按下按鈕時更新ContentTextBox的文字。

--- 2024_WpfApp7/MainWindow.xaml.cs
+++ 2024_WpfApp7/MainWindow.xaml.cs
@@ -16,9 +16,16 @@ namespace _2024_WpfApp7
     /// </summary>
     public partial class MainWindow : Window
     {
+        string defultURL = "https://data.moenv.gov.tw/api/v2/aqx_p_432?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=ImportDate%20desc&format=JSON";
         public MainWindow()
         {
             InitializeComponent();
+            UrlTextBox.Text = defultURL;
+        }
+
+        private void GetAQIButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentTextBox.Text = "抓取資料中...";
         }
     }
 }
